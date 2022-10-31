a2cd437c7666f4beb8272e2c8b57ea3358e14539
shanto <shanto@shantos-MacBook-Pro.local>
2022-10-31 20:16:57 +0600

    dhggj

---
+++ star.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+  int i,j;
+  int n =1;
+  
+  for ( i = 1; i <=17; i++)
+  {
+    for (j = 1; j <= i; j++)
+    {
+      printf("*");
+    }
+    n++;
+    printf("\n");
+  }
+  
+}

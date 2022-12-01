1fe6e6a9ca9604f88968e3c32683683709645aab
shanto <hhassibul@gmail.com>
2022-12-01 23:52:43 +0600

    dhfsdlak

--- functio-recap/lab/max-min.c
+++ functio-recap/lab/max-min.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+void max_min(int n,int num[],int *p,int *q){
+*p = num[0];
+*q = num[0];
+for (int i = 0; i < n; i++)
+{
+  if (num[i]> *p)
+  {
+    *p = num[i];
+  }
+  if (num[i]< *q)
+  {
+    *q = num[i];
+  }
+
+}
+
+}
 int main(){
-
-
+int ara[]= {3,5,6,2,1,7,8};
+int n=7;
+int maxx,minn;
+max_min(n,ara,&maxx,&minn);
+printf("%d %d",maxx,minn);
   return 0;
 }    
---
+++ functio-recap/lab/sum-prime.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int is_prime(int b){
+for (int i = 2; i < b; i++)
+{
+  if (b%i == 0)
+  {
+    return 0;
+  }
+  
+}
+return 1;
+}
+int sum(int a){
+int summ = 0;
+while (a>0)
+{
+  summ += a%10;
+  a= a/10;
+}
+return summ;
+}
+int main(){
+  int n;
+  scanf("%d",&n);
+  if (is_prime(sum(n))== 1)
+  {
+    printf("shantos number");
+  }else{
+    printf("no");
+  }
+  
+  
+  return 0;
+}

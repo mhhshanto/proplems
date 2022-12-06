50e5220e59383e54c5fc73ed3651ac0bfe221660
shanto <hhassibul@gmail.com>
2022-12-07 00:11:39 +0600

    done

--- structure/1.c
+++ structure/1.c
@@ -1,1 +1,16 @@
-   
+#include <stdio.h>
+#include <string.h>
+struct student
+{
+  int roll;
+  char name[50];
+  int age;
+  float weight;
+};
+
+int main(){
+  struct student s[10];
+ s[0]
+
+  printf("%s",shanto.name);
+}

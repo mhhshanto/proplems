8e6926ba9b4ecd87612d25f1427e0a412f849847
shanto <hhassibul@gmail.com>
2022-12-09 23:45:52 +0600

    ok

--- 2.c
+++ 2.c
@@ -1,1 +1,37 @@
-    
+#include <stdio.h>
+// struct Point
+// {
+//  float x;
+//  float y;
+// };
+// struct Point mid(struct Point p1,struct Point p2){
+// struct Point res;
+// res.x = (p1.x + p2.x)/2;
+// res.y = (p1.y + p2.y)/2;
+// return res;
+// }
+// int main(){
+// struct Point p1 = {1,1};
+// struct Point p2 = {4,5};
+// struct Point p3;
+// p3 = mid(p1,p2);
+// printf("%.2f ,%.2f",p3.x,p3.y);
+// }
+struct point
+{
+  float x;
+  float y;
+};
+struct point mid(struct point p,struct point q){
+  struct point ans;
+  ans.x = (p.x+q.x)/2;
+  ans.y = (p.y + q.y)/2;
+  return ans;
+}
+int main(){
+  struct point p ={1,1};
+  struct point q ={4,5};
+  struct point s;
+  s = mid(p,q);
+  printf("%.2f,%.2f ",s.x,s.y);
+}
---
+++ 3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+struct point
+{
+  float x;
+  float y;
+};
+struct triangle{
+  struct point A;
+  struct point B;
+  struct point C;
+}
+struct point centroid(struct triangle t)
+{
+  struct point ans;
+  ans.x = (t.A.x+t.B.x+t.C.x);
+  ans.x = (t.A.y +t.B.y+t.C.y);
+  return ans;
+};
+
+int main(){
+  struct point p1 ={1,1};
+  struct point p2 ={4,5};
+  struct point p3 ={1,5};
+  struct triangle t = {p1,p2,p3};
+  struct point c = centroid(t);
+
+}

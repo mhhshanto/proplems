1993938cbb025e97815ff8d123fa41596d28ee64
shanto <hhassibul@gmail.com>
2022-12-16 00:01:23 +0600

    file

---
+++ TicTacToe/tictactoe.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void printCell(int a[][4],int n){
+for (int i = 1; i <= n; i++)
+{
+  for (int j = 1; j <= n; j++)
+  {
+    if (a[i][j]== -1)
+    {
+      printf(" ");
+    }
+    
+    if (j<n)printf("\t | \t"); 
+  }
+  printf("\n");
+  if (i<n)
+  {
+    printf("___________________________________");
+  }
+  
+  printf("\n\n");
+}
+}
+int main(){
+  int n = 3;
+  int a[4][4];
+for (int i = 1; i <= n; i++)
+{
+  for (int j = 1; j <= n; j++)
+  {
+    a[i][j]= -1;
+  }
+  
+}
+printCell(a,n);
+  return 0;
+}

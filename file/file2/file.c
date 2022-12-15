bc2e2412f4bf0f374cff517597f7e9b54c4b4cfe
shanto <hhassibul@gmail.com>
2022-12-15 19:14:56 +0600

    file

--- file/file2/file.c
+++ file/file2/file.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
 int main(){
+  FILE *input;
+  FILE *output;
+  input = fopen("input.txt","r");
+  output = fopen("output.txt","w");
+  if (input == NULL)
+  {
+    fprintf(output,"no file found");
+    return 0;
+  }
+  int n;
+  int sum = 0;
+  fscanf(input,"%d",&n);
+  for (int i = 0; i < n; i++)
+  {
+    int a;
+    fscanf(input,"%d",&a);
+    sum += a;
+  }
+  fprintf(output,"%d",sum);
+  
+  
   return 0;
 } 

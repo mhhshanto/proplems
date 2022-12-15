57f69274ef97e2d1a9a0306a75c37d750bab36a4
shanto <hhassibul@gmail.com>
2022-12-15 18:00:54 +0600

    file

--- file/file.c
+++ file/file.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
+// int main(){
+//   FILE *inputFile;
+//   inputFile = fopen("input.txt","r");
+//   if (inputFile == NULL)
+//   {
+//     printf("no file found");
+//   }
+//   FILE *outputFile;
+//   outputFile = fopen("output.txt","w");
+//   while (1)
+//   {
+//     char ch = fgetc(inputFile);
+//     if (ch == EOF)
+//     {
+//       break;
+//     }
+//     fputc(ch,outputFile);
+//   }
+  
+
+//   return 0;
+// }
 int main(){
-  int n;
-  scanf("%d",&n);
-  int sum = 0;
-  for (int i = 0; i < n; i++)
+  FILE *input;
+  input = fopen("input.txt","r");
+  if (input == NULL)
   {
-    int a;
-    scanf("%d",&a);
-    sum+=a;
+    printf("no file found");
   }
-  printf("sum--> %d",sum);
-  return 0;
+  FILE *output;
+  output = fopen("output.txt","w");
+  while (1)
+  {
+    char ch = fgetc(input);
+    if (ch == EOF)
+    {
+      break;
+    }
+    fputc(ch,output);
+  }
+  
 }
